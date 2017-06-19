ba42a4470ca8e410ee1a5825d1783626259f134a
cbhramar <bhramar115@gmail.com>
2017-06-19 23:51:29 +0530

    Adding Cesaro Tear

--- CesaroTear.cpp
+++ CesaroTear.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 void cesaroTear(double x, double y, double t, double l, double order, double theta);
 
-int main(){
+int main() {
     initCanvas("Cesaro Tear", 1360, 700);
     int order = 4;
     double t = PI/20;
@@ -25,21 +25,13 @@ int main(){
     return 0;
 }
 
-void cesaroTear(double x, double y, double t, double l, double order, double theta){
-    if (order == 0){
+void cesaroTear(double x, double y, double t, double l, double order, double theta) {
+    if (order == 0) {
         Line l0(x, y, x + cos(theta)*l, y + sin(theta)*l);
         l0.imprint();
     }
     else {
         double d = sin(t/2)*l/2;
-/*        Line l0(x - cos(t)*l/2, y - sin(t)*l/2, x - cos(t)*d/2, y - sin(t)*d/2);
-        l0.imprint();
-        Line l1(x + cos(t)*l/2, y + sin(t)*l/2, x + cos(t)*d/2, y + sin(t)*d/2);
-        l1.imprint();
-        Line l2(x - cos(t)*l/2, y - sin(t)*l/2, x - sin(theta)*l/2, y + cos(theta)*l/2);
-        l2.imprint();
-        Line l3(x - sin(theta)*l/2, y + cos(theta)*l/2, x + cos(t)*d/2, y + sin(t)*d/2);
-        l3.imprint(); */
         cesaroTear(x, y, t, l/2 - d, order - 1, theta);
         cesaroTear(x + cos(theta)*(l/2 + d), y + sin(theta)*(l/2 + d), t, l/2 - d, order - 1, theta);
         cesaroTear(x + cos(theta)*(l/2 - d), y + sin(theta)*(l/2 - d), t, l/2, order - 1, theta + PI/2 - t/2);
